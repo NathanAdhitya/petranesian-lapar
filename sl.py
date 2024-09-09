5df539bc3b7dd5530f53f1430167572451f79a9b
Nathan Adhitya <c14220205@john.petra.ac.id>
2024-09-09 14:43:48 +0000

    improved accuracy, maybe

--- sl.py
+++ sl.py
@@ -4,6 +4,14 @@ from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.readers.file import CSVReader
 from llama_index.core.llms import ChatMessage, MessageRole
+from llama_index.core import Settings
+
+import sys
+
+import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 system_prompt = """
 You are a multi-lingual expert system who has knowledge, based on 
@@ -11,7 +19,7 @@ real-time data. You will always try to be helpful and try to help them
 answering their question. If you don't know the answer, say that you DON'T
 KNOW.
 
-Bahasa utama yang Anda gunakan adalah Bahasa Indonesia.
+Try to always reply in Bahasa Indonesia.
 Tugas Anda adalah untuk menjadi pelayan kantin yang ramah.
 Kantin yang Anda layani adalah kantin kampus Universitas Kristen Petra Surabaya.
 Pada Universitas Kristen Petra terdapat 5 gedung utama yang setiap gedungnya memiliki kantin, 
@@ -23,28 +31,38 @@ yang relevan berdasarkan kebutuhan mereka.
 
 Untuk setiap jawaban, pastikan Anda memberikan detil yang lengkap.
 
-Contoh:
+Contoh percakapan:
 User: Aku lapar, makan mie di mana ya?
 Assistant: Kamu lagi di gedung mana?
 User: Gedung P
 Assistant: Di Gedung P terdapat 2 stall yang menjual mie goreng. Ada Ndokee Express dan Soto Ayam Jago. Harga mie goreng di Ndokee Express sekitar 12.000 rupiah, sementara di soto ayam jago sekitar 16.000 rupiah.
+
+Percakapan sejauh ini:
 """
 
-llm = Ollama(model="llama3.1:latest", base_url="http://127.0.0.1:11434", system_prompt=system_prompt)
-embeddings = OllamaEmbedding(base_url="http://127.0.0.1:11434", model_name="mxbai-embed-large:latest")
+Settings.llm = Ollama(model="llama3.1:latest", base_url="http://127.0.0.1:11434", system_prompt=system_prompt)
+Settings.embed_model = OllamaEmbedding(base_url="http://127.0.0.1:11434", model_name="mxbai-embed-large:latest")
 
-@st.cache_resource()
+
+@st.cache_resource(show_spinner="Mempersiapkan data kantin – sabar ya.")
 def load_data(vector_store=None):
     with st.spinner(text="Mempersiapkan data kantin – sabar ya."):
-        csv_parser = CSVReader()
+        csv_parser = CSVReader(concat_rows=False)
         file_extractor = {".csv": csv_parser}
 
         # Read & load document from folder
-        reader = SimpleDirectoryReader(input_dir="./docs", recursive=True, file_extractor=file_extractor)
+        reader = SimpleDirectoryReader(
+            input_dir="./docs",
+            recursive=True,
+            file_extractor=file_extractor,
+
+            # Suppress file metadata, not sure if this works or not.
+            file_metadata=lambda x: {}
+        )
         documents = reader.load_data()
 
     if vector_store is None:
-        index = VectorStoreIndex.from_documents(documents)
+        index = VectorStoreIndex.from_documents(documents, show_progress=True)
     return index
 
 
@@ -52,18 +70,29 @@ def load_data(vector_store=None):
 st.title("Petranesian Lapar 🍕")
 index = load_data()
 
-# Initialize the chat engine
-if "chat_engine" not in st.session_state.keys():
-    st.session_state.chat_engine = index.as_chat_engine(
-        chat_mode="condense_plus_context", verbose=True, streaming=True,
-    )
-
 # Initialize chat history if empty
 if "messages" not in st.session_state:
     st.session_state.messages = [
         {"role": "assistant",
          "content": "Halo! Lagi mau makan/minum apaan? 😉"}
     ]
+
+# Initialize the chat engine
+if "chat_engine" not in st.session_state.keys():
+    # Initialize with custom chat history
+    init_history = [
+        ChatMessage(role=MessageRole.ASSISTANT, content="Halo! Lagi mau makan/minum apaan? 😉"),
+    ]
+    st.session_state.chat_engine = index.as_chat_engine(
+        chat_mode="condense_plus_context", verbose=True, streaming=True,
+        system_prompt=system_prompt,
+        context_prompt=(
+                "Anda adalah pelayan kantin yang ramah yang dapat membantu user mencari makan dan minum."
+                "Ini adalah dokumen yang relevan terhadap konteks:\n"
+                "{context_str}"
+                "\nInstruksi: Gunakan riwayat obrolan sebelumnya, atau konteks di atas, untuk berinteraksi dan membantu pengguna."
+            )
+    )
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
